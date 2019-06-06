4ea851b60caa96317d061a8c50d50e5057d9ed02
wjk <q523969705@gmail.com>
2019-06-06 18:21:53 +0800

    fix

--- src/main/java/top/ingxx/security/config/SpringSecurityConfig.java
+++ src/main/java/top/ingxx/security/config/SpringSecurityConfig.java
@@ -6,23 +6,20 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import top.ingxx.security.filter.MyRequestFilter;
 import top.ingxx.security.provider.MyProvider;
-import top.ingxx.security.service.Myservice;
+import top.ingxx.security.service.MyService;
 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private Myservice myservice;
+    private MyService myService;
 
     @Autowired
     private MyProvider myProvider;
--- src/main/java/top/ingxx/security/service/MyService.java
+++ src/main/java/top/ingxx/security/service/MyService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Myservice implements UserDetailsService {
+public class MyService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return User.builder().username("user").password("$2a$10$pBkZ18ezHvTc2cNBaN8k/e36JUauv/s5KIwqj/PRMS0hIHSIsvnIq").roles("USER").build();
